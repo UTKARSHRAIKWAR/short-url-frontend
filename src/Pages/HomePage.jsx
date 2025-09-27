8abb5a1f840898a87ab5d80a664b956b06a64540
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-09-28 01:33:50 +0530

    test

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -23,7 +23,7 @@ const HomePage = () => {
         <CardHeader>
           {/* CardTitle can be used for "Login / Sign Up" if you wish */}
           <CardTitle className="text-center text-2xl font-bold">
-            Welcome
+            Welcome test
           </CardTitle>
         </CardHeader>
         <CardContent>
